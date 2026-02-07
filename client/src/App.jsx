b9ba935e1117ed449a864b1a4c68b93d0dfaa63f
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-02-07 16:58:57 +0300

    Finsihed Login and sign-up authentication via clerk, Loading and Sidebar Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
-import Login from "./pages/Login.jsx"
+import Login from "./pages/login.jsx"
 import Feed from './pages/Feed.jsx'
 import Messages from './pages/Messages.jsx'
 import ChatBox from './pages/ChatBox.jsx'
@@ -8,13 +8,16 @@ import Connections from './pages/Connections.jsx'
 import Discover from './pages/Discover.jsx'
 import Profile from './pages/Profile.jsx'
 import CreatePost from './pages/CreatePost.jsx'
+import {useUser} from '@clerk/clerk-react'
+import Layout from "./pages/Layout.jsx"
 
 
 const App = () => {
-  retrun (
+  const {user} = useUser()
+  return (
     <>
       <Routes>
-        <Route path='/' element ={<Login/>}>
+        <Route path='/' element ={!user ? <Login/> : <Layout/>}>
           <Route index element={<Feed/>}/>
           <Route path = 'messages' element={<Messages/>}/>
           <Route path = 'messages/:userId' element={<ChatBox/>}/>
